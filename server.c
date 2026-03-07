66a7d80b6372f8f0207fc90b84a59ae4d6c91013
Anshuman22 <kumaranshuman2273@gmail.com>
2026-03-07 15:44:38 +0530

    more modular version of the same code

--- server.c
+++ server.c
@@ -15,6 +15,7 @@ int MAX_QUEUE_LEN = 5; //max length of queue for the pending connection for sock
 size_t SIZE = 512; // size for read
 
 enum http_state_t {
+    INIT,
     READING_HEADERS,
     HEADERS_COMPLETE,
     READING_BODY,
@@ -23,20 +24,41 @@ enum http_state_t {
     ERROR,
 };
 
-struct header_metadata_t {
+struct connection_t {
+    struct sockaddr_in addr;
+    socklen_t addr_len;
+    //ssize_t recv_ret;
+    int socket_file_des;
+    //int ret;
+    int accept_fd;
+};
+
+struct http_req_t {
+    char *header;
+    size_t remaining;
+    uint append_size;
+    uint32_t header_read_size;
+    uint32_t start_pointer_for_header_completion_scan;
+    bool header_flag;
     long content_length;
     int cl_count;
     bool is_chunked;
 };
 
-bool is_header_complete(char *header, uint32_t header_read_size, uint32_t start_index) {
-    for (uint32_t i = start_index; (i + 3) < header_read_size; i++) {
-        if (header[i] == '\r' &&
-            header[i+1] == '\n' &&
-            header[i+2] == '\r' &&
-            header[i+3] == '\n') return true;
+enum http_state_t machine_state = INIT;
+
+void is_header_complete(struct http_req_t *req) {
+    for (uint32_t i = req->start_pointer_for_header_completion_scan; (i + 3) < req->header_read_size; i++) {
+        if ((req->header)[i] == '\r' &&
+            (req->header)[i+1] == '\n' &&
+            (req->header)[i+2] == '\r' &&
+            (req->header)[i+3] == '\n') {
+            
+            req->header_flag = true;
+            return;
+        }
     }
-    return false;
+    return;
 }
 void skip_to_nline(char* header, uint32_t *index, uint32_t header_read_size) {
     for (uint32_t i = *index; i < header_read_size; i++) {
@@ -48,7 +70,7 @@ void skip_to_nline(char* header, uint32_t *index, uint32_t header_read_size) {
     }
 }
 
-long search_cl(char* header, uint32_t *index, uint32_t header_read_size) {
+long search_cl(char* header, uint32_t header_read_size, uint32_t *index) {
     if (tolower(header[*index]) == 'c' &&
         tolower(header[(*index)+1]) == 'o' &&
         tolower(header[(*index)+2]) == 'n' &&
@@ -82,7 +104,7 @@ long search_cl(char* header, uint32_t *index, uint32_t header_read_size) {
     }
 }
 
-int search_te(char* header, uint32_t *index, uint32_t header_read_size) {
+bool search_te(char* header, uint32_t header_read_size, uint32_t *index) {
     if (tolower(header[*index]) == 't' &&
         tolower(header[(*index)+1]) == 'r' &&
         tolower(header[(*index)+2]) == 'a' &&
@@ -115,164 +137,191 @@ int search_te(char* header, uint32_t *index, uint32_t header_read_size) {
             tolower(header[(*index)+6]) == 'd' ) {
             
             *index = (*index) + 7;
-            return 1;
+            return true;
         }
     }
-    return 0;
+    return false;
 }
-struct header_metadata_t shallow_search(char *header, uint32_t header_read_size) {
-    struct header_metadata_t metadata = {0};
-    //long cl = 0;
+
+void shallow_search(struct http_req_t *req) {
+    long cl = 0;
     uint32_t index = 0;
-    //int cl_count = 0;
-    //int te_count = 0;
-    while (index < header_read_size) {
+    int cl_count = 0;
+    bool is_chunked = false;
+
+    char *header = req->header;
+    uint32_t header_size = req->header_read_size;
+
+    while (index < header_size) {
         while (header[index] == ' ') index++;
         if (header[index] == 'c' || header[index] == 'C') {
-            metadata.content_length = search_cl(header, &index, header_read_size);
-            if (metadata.content_length) (metadata.cl_count)++;
+            cl = search_cl(header, header_size, &index);
+            if (cl) (cl_count)++;
         }
         if (header[index] == 't' || header[index] == 'T')
-            metadata.is_chunked += search_te(header, &index, header_read_size);
+            is_chunked = search_te(header, header_size, &index);
 
-        else skip_to_nline(header, &index, header_read_size);
+        else skip_to_nline(header, &index, header_size);
         printf("Index: %d\n", index);
     }
 
-    printf("\ncontent-length parsed: %ld\n", metadata.content_length);
+    printf("\ncontent-length parsed: %ld\n", cl);
     
-    return metadata;
+    req->content_length = cl;
+    req->cl_count = cl_count;
+    req->is_chunked = is_chunked;
 }
 
-int main(int argc, char* argv[]) {
-    struct sockaddr_in addr; //addr for bind
+void socket_stuff(struct connection_t *connection) {
+    connection->socket_file_des = socket(AF_INET, SOCK_STREAM, 0);
 
-    
-    int socket_file_des = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (socket_file_des < 0) {
+    if (connection->socket_file_des < 0) {
         perror("[!]socket() failed!!!");
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(PORT);
+    connection->addr.sin_family = AF_INET;
+    connection->addr.sin_addr.s_addr = INADDR_ANY;
+    connection->addr.sin_port = htons(PORT);
 
-    socklen_t addr_len = sizeof(addr);
+    connection->addr_len = sizeof(connection->addr);
 
-    int bind_ret = bind(socket_file_des, (const struct sockaddr *)&addr, addr_len);
+    int ret = bind(connection->socket_file_des, (const struct sockaddr *)&(connection->addr), connection->addr_len);
 
-    if (bind_ret < 0) {
+    if (ret < 0) {
         perror("[!]bind() failed!!!");
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
-    int listen_ret = listen(socket_file_des, MAX_QUEUE_LEN);
+    ret = listen(connection->socket_file_des, MAX_QUEUE_LEN);
 
-    if (listen_ret < 0) {
+    if (ret < 0) {
         perror("[!]listen() failed!!!");
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
     
     printf("Ready to accept\n");
+    return;
+}
+
+struct http_req_t* http_header_read(struct connection_t *connection) {
+    char *iheader = (char*)malloc(HEADER_SIZE);
+    if (iheader == NULL) {
+        perror("[!]malloc() failed...");
+        close(connection->accept_fd);
+        return NULL;
+    }
+
+    machine_state = READING_HEADERS;
+
+    struct http_req_t *req = (struct http_req_t*)malloc(sizeof(struct http_req_t));
+    *req = (struct http_req_t){0};
+    req->header = iheader;
+
+    while (req->header_read_size < HEADER_SIZE) {
+        req->remaining = HEADER_SIZE - req->header_read_size;
+        ssize_t recv_ret = recv(connection->accept_fd, ((req->header)+(req->append_size)), req->remaining, 0);
+
+        if (recv_ret < 0) {
+            perror("[!] recv() failed!!!");
+            return NULL;
+        }
+
+        req->append_size += recv_ret;
+        req->start_pointer_for_header_completion_scan = (req->header_read_size >= 3) ? req->header_read_size - 3 : 0;
+        req->header_read_size += recv_ret;
+
+        is_header_complete(req);
+        if (req->header_flag && machine_state == READING_HEADERS) machine_state = HEADERS_COMPLETE;
+
+        if (req->header_flag && machine_state == HEADERS_COMPLETE) {
+            printf("\nHeader complete....\n");
+            printf("header size: %d\n", req->header_read_size);
+
+            //search for content-length and transfer-encoding
+            shallow_search(req);
+            if (req->cl_count > 1) {
+                printf("\nerror 400: bad request(multiple content-length found)\n\n");
+                break;
+            }
+            if (req->cl_count && req->is_chunked) {
+                printf("\nboth content length and transfer-encoding: chunked found\n\n");
+                //break;
+            }
+            if (req->content_length < 0) {
+                printf("cl: %ld\nInvalid content-length\n", req->content_length);
+                break;
+            }
+            if (req->is_chunked) printf("\nthe request has chunked transfer-encoding\n\n");
+            if (req->content_length <= 0 && !(req->is_chunked)) {
+                printf("error 411: length required\n");
+                break;
+            }
+
+            machine_state = READING_BODY;
+        }
+
+        if (recv_ret == 0 && !(req->header_flag) && machine_state == READING_HEADERS) {
+            printf("\nConnection closed but header is incomplete\ncan't parse the header....\nclosing the connection\n");
+            close(connection->accept_fd);
+            break;
+        }
+
+        if (recv_ret == 0 && req->header_flag && machine_state == READING_BODY) {
+            printf("\nconnection closed...\nheader is complete but body is incomplete\ncan't process the req...\nclosing the connection\nif both is complete process req and send response.\n");
+            close(connection->accept_fd);
+            break;
+        }
+
+        write(1, req->header, req->header_read_size);            
+        //header and its shenanigans are complete. its time to read body 
+        if(machine_state == READING_BODY) break;
+    }
+    return req;
+}
+
+int main(int argc, char* argv[]) {
+    struct connection_t *connection = (struct connection_t*)malloc(sizeof(struct connection_t));
+    if (connection == NULL) {
+        perror("[!] malloc failed for connection_t");
+        exit(EXIT_FAILURE);
+    }
+
+    socket_stuff(connection);
 
     while(1) {
-        int accept_fd = accept(socket_file_des, (struct sockaddr *)&addr, &addr_len);
+        connection->accept_fd = accept(connection->socket_file_des,
+                                       (struct sockaddr *)&(connection->addr),
+                                       &(connection->addr_len));
 
-        if (accept_fd < 0) {
+        if (connection->accept_fd < 0) {
             perror("[!]accept() failed!!!");
-            exit(-1);
-        }
-        char *header = (char*)malloc(HEADER_SIZE);
-        if (header == NULL) {
-            perror("[!]malloc() failed...");
-            close(accept_fd);
             continue;
         }
 
-        enum http_state_t machine_state = READING_HEADERS;
-        struct header_metadata_t metadata = {0};
-
-        uint append_size = 0; 
-        uint32_t header_read_size = 0; //to count how many bytes we have read yet
-        
-        while (header_read_size < HEADER_SIZE) {
-            size_t remaining = HEADER_SIZE - header_read_size;
-            ssize_t recv_ret = recv(accept_fd, (header+append_size), remaining, 0);
-            
-            if (recv_ret < 0) {
-                perror("[!] recv() failed!!!");
-                exit(-1);
-            }
-            
-            append_size += recv_ret;
-            uint32_t start_pointer_for_header_completion_scan = (header_read_size >= 3) ? header_read_size - 3 : 0;
-            header_read_size += recv_ret;
-
-            bool header_flag = is_header_complete(header, header_read_size, start_pointer_for_header_completion_scan);
-            if (header_flag && machine_state == READING_HEADERS) machine_state = HEADERS_COMPLETE;
-
-            if (header_flag && machine_state == HEADERS_COMPLETE) {
-                printf("\nHeader complete....\n");
-                printf("header size: %d\n", header_read_size);
-
-                //search for content-length and transfer-encoding
-                metadata = shallow_search(header, header_read_size);
-                if (metadata.cl_count > 1) {
-                    printf("\nerror 400: bad request(multiple content-length found)\n\n");
-                    break;
-                }
-                if (metadata.cl_count && metadata.is_chunked) {
-                    printf("\nboth content length and transfer-encoding: chunked found\n\n");
-                    //break;
-                }
-                if (metadata.content_length < 0) {
-                    printf("cl: %ld\nInvalid content-length\n", metadata.content_length);
-                    break;
-                }
-                if (metadata.is_chunked) printf("\nthe request has chunked transfer-encoding\n\n");
-                if (metadata.content_length <= 0 && !metadata.is_chunked) {
-                    printf("error 411: length required\n");
-                    break;
-                }
-
-                machine_state = READING_BODY;
-            }
-
-            if (recv_ret == 0 && !header_flag && machine_state == READING_HEADERS) {
-                printf("\nConnection closed but header is incomplete\ncan't parse the header....\nclosing the connection\n");
-                close(accept_fd);
-                break;
-            }
-
-            if (recv_ret == 0 && header_flag && machine_state == READING_BODY) {
-                printf("\nconnection closed...\nheader is complete but body is incomplete\ncan't process the req...\nclosing the connection\nif both is complete process req and send response.\n");
-                close(accept_fd);
-                break;
-            }
-
-            write(1, header, header_read_size);            
-            //header and its shenanigans are complete. its time to read body 
-            if(machine_state == READING_BODY) break;
+        struct http_req_t *req = http_header_read(connection);
+        if(req == NULL) {
+            printf("\n\n[!]Fatal error while reading header\n\n");
+            continue;
         }
         
-        if (machine_state == READING_BODY && !metadata.is_chunked) {
-            char *body_buffer = (char*)malloc(metadata.content_length);
+        if (machine_state == READING_BODY && !(req->is_chunked)) {
+            char *body_buffer = (char*)malloc(req->content_length);
             if (body_buffer == NULL) {
                 perror("[!] malloc failed for body...\n");
                 continue;
             }
-
-            ssize_t body_bytes_read = 0;
-            while (body_bytes_read < metadata.content_length) {
-                ssize_t recv_ret = recv(accept_fd, body_buffer+body_bytes_read, metadata.content_length - body_bytes_read, 0);
-                body_bytes_read += recv_ret;
-            }
+            printf("Reached body reading\n\n");
+            break; //delete this
+            //ssize_t body_bytes_read = 0;
+            //while (body_bytes_read < req->content_length) {
+            //    ssize_t recv_ret = recv(connection->accept_fd, body_buffer+body_bytes_read, req->content_length - body_bytes_read, 0);
+            //    body_bytes_read += recv_ret;
+            //}
         }
 
-        close(accept_fd);
+        close(connection->accept_fd);
     }
-    close(socket_file_des);
+    close(connection->socket_file_des);
     return 0;
 }
